33673e1a96008d2068cf6ae7d76de2bf4b96a708
Zachary Wentworth <Zachary Wentworth>
2016-04-28 02:45:29 -0700

    commit failed, resub

---
+++ IntoTheFurther/Assets/CameraPos.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPos : MonoBehaviour {
+
+    public Camera cam;
+
+    void OnDrawGizmos()
+    {
+
+        float height = 2f * cam.orthographicSize;
+        float width = height * cam.aspect;
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position, new Vector3(width, height, 1));
+    }
+}
---
+++ IntoTheFurther/Assets/PlayerControl.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+    public float speed = 15.0f;
+    float yMax = 0f;
+    float yMin = -4.5f;
+
+    void Start() {
+          
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            //transform.position += new Vector3(0, -speed * Time.deltaTime, 0);
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            //transform.position += new Vector3(0, +speed * Time.deltaTime, 0);
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+
+        //restrict player to game space
+        float newY = Mathf.Clamp(transform.position.y, yMin, yMax);
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+    }
+
+}
