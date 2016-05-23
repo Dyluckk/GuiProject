8e57c434430720c3b5b3e74dfb2acb99112d5d79
Zachary Wentworth <Zachary Wentworth>
2016-05-23 14:55:07 -0700

    fixed file hierarchy, fixed a few scripts for global settings in game

---
+++ IntoTheFurther/Assets/scenes/Menu/scripts/GlobalOptions.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GlobalOptions : MonoBehaviour
+{
+
+    public float volume;
+
+    //make sure volume is set when the new scene is loaded
+    //void OnLevelWasLoaded()
+    //{
+    //    AudioListener.volume = PlayerPrefs.GetFloat("volume");
+    //    DontDestroyOnLoad(gameObject);
+    //}
+
+    void Start()
+    {
+        AudioListener.volume = PlayerPrefs.GetFloat("volume");
+        DontDestroyOnLoad(gameObject);        
+    }
+
+    void Update()
+    {
+        volume = PlayerPrefs.GetFloat("volume");
+        AudioListener.volume = PlayerPrefs.GetFloat("volume");
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireCube(transform.position, new Vector3(1, 1, 1));
+    }
+
+
+}
---
+++ IntoTheFurther/Assets/scenes/TheWall/scripts/TheWall.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TheWall : MonoBehaviour {
+
+    public Camera cam;
+    public GameObject GlobalOptions;
+    public AudioSource[] BGM;
+    public AudioSource[] SFX;
+
+    void Start()
+    {
+        GlobalOptions = GameObject.Find("GlobalOptions");
+        AudioListener.volume = PlayerPrefs.GetFloat("volume");
+        BGM[0].volume = GlobalOptions.GetComponent<GlobalOptions>().volume;
+        SFX[0].volume = GlobalOptions.GetComponent<GlobalOptions>().volume;
+    }
+
+    void OnDrawGizmos()
+    {
+
+        float height = 2f * cam.orthographicSize;
+        float width = height * cam.aspect;
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position, new Vector3(width, height, 1));
+    }
+}
